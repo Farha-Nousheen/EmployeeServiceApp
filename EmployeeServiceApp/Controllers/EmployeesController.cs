480e043abf3d2e683b40df5009f7fd4930778b07
farha <farha.nousheen@quadrantresource.com>
2021-08-30 09:59:43 +0530

    Add project files.

---
+++ EmployeeServiceApp/Controllers/EmployeesController.cs
@@ -0,0 +1,96 @@
+﻿using EmployeeServiceApp.Data;
+using EmployeeServiceApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeServiceApp.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        EmployeeContext _context;
+        public EmployeesController(EmployeeContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult CreateEmployee(Employee emp)
+        {
+            _context.Employee.Add(emp);
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpGet]
+        public List<Employee> GetEmployee()
+        {
+            var emp = _context.Employee.ToList();
+            return emp;
+        }
+        // PUT: api/Employees/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public IActionResult UpdateEmployee(Employee emp)
+        {
+            var result = _context.Employee.FirstOrDefault(e => e.Id == emp.Id);
+            if (result != null)
+            {
+                result.Age = emp.Age;
+                result.Name = emp.Name;
+                result.Country = emp.Country;
+                result.Designation = emp.Designation;
+                result.ContactNumber = emp.ContactNumber;
+                _context.SaveChanges();
+                return Ok();
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public string Getusername(string Email)
+        {
+            var name = Email.Split('@')[0];
+            return name;
+        }
+        // GET: api/Employees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            var employee = await _context.Employee.FindAsync(id);
+
+
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+
+
+            return employee;
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employee.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employee.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EmployeeServiceApp/Controllers/StringController.cs
@@ -0,0 +1,91 @@
+﻿using EmployeeServiceApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeServiceApp.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class StringController : ControllerBase
+    {
+
+        EmployeeContext _context;
+        public StringController(EmployeeContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public string Stringreverse(string s)
+
+
+
+        {
+            string result = string.Join(' ', s
+                .Split(' ').
+                Select(x => new string(x.Reverse().ToArray())));
+            return result;
+        }
+        [HttpGet]
+        public string Namingsplit(string name)
+        {
+            string first = name.Split(' ')[0];
+            return first;
+        }
+        [HttpGet]
+        public string convertnameUpper(string txt)
+        {
+            string txt1 = txt.ToUpper();
+            return txt1;
+        }
+        [HttpGet]
+        public string convertnameLower(string txt)
+        {
+            string txt1 = txt.ToLower();
+            return txt1;
+        }
+        [HttpGet]
+        public bool Comparing(string txt, string txt1)
+        {
+            if (string.Compare(txt, txt1) == 0)
+            {
+                return true;
+            }
+
+
+
+            else { }
+            return false;
+        }
+        [HttpGet]
+        public int leng(string str)
+        {
+            int str1 = str.Length;
+            return str1;
+        }
+        [HttpGet]
+        public string concat(string str1, string str2)
+        {
+            string name = string.Concat(str1, str2);
+            return name;
+        }
+        [HttpGet]
+        public int IndexOf(string name,char c)
+        {
+         
+            return name.IndexOf(c);
+        }
+
+
+        }
+        
+
+            
+    }
+
+
+    
+
